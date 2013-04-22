a876241e170c71752b354c4c9932742ed8b30eee
= <=mukizaa@yahoo.com>
2013-04-22 11:39:21 +0300

    Implemented the roll method on dice

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,4 +1,5 @@
 class Dice
+
   def initialize
     @face = 1
   end
@@ -8,9 +9,19 @@ class Dice
   end
 
   def face=(face)
-    raise(ArgumentError, 'Invalid Face') if face > 6
-
+    raise(ArgumentError, 'Invalid Face') unless face.between?(1,6) and face.is_a? Integer
     @face = face
+  end
+
+  def roll
+    @face = range_rand(1, 6)
+    return self
+  end
+
+  private
+
+  def range_rand(min,max)
+    min + Kernel.rand(max-min)
   end
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -7,10 +7,24 @@ describe 'Dice' do
 
   it 'Should  not be greater than 6' do
     expect{dice.face = 7}.to raise_error(ArgumentError)
-    end
+  end
+
+  it 'should not be less than 1' do
+    expect{dice.face = 0}.to raise_error(ArgumentError)
+  end
 
   it 'can  be set to a face' do
     dice.face = 6
     expect(dice.face).to eq(6)
   end
+
+  it 'should be a numeric' do
+    expect{dice.face = 1.5}.to raise_error(ArgumentError)
+  end
+
+  it 'should roll' do
+    Kernel.stub!(:rand).and_return(3)
+    expect(dice.roll.face).to eq(4)
+  end
+
 end
