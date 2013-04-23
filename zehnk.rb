19a667d8457f90a675619ebc69d38604c5c3eb9f
Felix Kitaka <webash.uganda@gmail.com>
2013-04-23 17:04:47 +0300

    added the display diceset helper

--- zehnk.rb
+++ zehnk.rb
@@ -1,9 +1,25 @@
 require 'sinatra'
 require './lib/dice'
+require './lib/dice_set'
+require './lib/game'
 require 'haml'
 
+helpers do
+  def display_dice_set(dice_set)
+    output = []
+    dice_set.dices.each do |dice|
+       output << display_dice(dice)
+    end
+    output.join ''
+  end
+
+  def display_dice(dice)
+    "<img src='images/dice_#{dice.face}.png'/>"
+  end
+end
+
 get '/' do
-  dice = Dice.new
-  dice.roll
-  haml :index, :locals => { :dice => dice}
+  game  = Game.new
+  game.start
+  haml :index, :locals => { :game => game}
 end
