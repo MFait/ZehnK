f04af7cb4c74d0153f4f1d603449ad5a8b8abf01
= <=mukizaa@yahoo.com>
2013-04-23 13:48:18 +0300

    Created overide for the plus

--- lib/dice_set.rb
+++ lib/dice_set.rb
@@ -10,23 +10,34 @@ class DiceSet
     @dices.count
   end
 
-  def add(dice)
-    @dices << dice
+  def add(dices)
+    dices = [dices] unless dices.is_a?(Array)
+
+    dices.each { |dice| @dices << dice }
     self
   end
 
 
-  #def take(dice_indicies)
-  #  taken_dice_set = DiceSet.new
-  #  dice_indicies.each do |index|
-  #    taken_dice_set.add(@dices[index])
-  #    @dices.delete_at(index)
-  #  end
-  #  taken_dice_set
-  #end
+  def take(indices)
+    taken_dice_set = DiceSet.new
+    left_dices = []
+
+    self.dices.each_with_index do |_, index|
+      indices.include?(index) ? taken_dice_set.add(@dices[index]) : left_dices << @dices[index]
+    end
+
+    @dices = left_dices
+    taken_dice_set
+  end
+
   def ==(other)
     return false unless self.count == other.count
-    self.dices.clone.sort - other.dices.sort == []
+    self.dices.clone.sort - other.dices.clone.sort == []
+  end
+
+  def +(other)
+    merged_dices = self.dices.clone.concat(other.dices.clone)
+    DiceSet.new.add(merged_dices)
   end
 
 end
--- spec/dice_set_spec.rb
+++ spec/dice_set_spec.rb
@@ -18,12 +18,28 @@ describe 'DiceSet' do
 
   it { dice_set_with([1,2,3,4]).should_not eq(dice_set_with([5,6,1,2])) }
 
-  #it 'should take dice from diceset' do
-  #  dice_set.add(dice.roll).add(dice.roll).add(dice.roll)
-  #  taken_dice_set = dice_set.take([1,2])
-  #  taken_dice_set.count.should eq(2)
-  #  dice_set.count.should eq(1)
-  #end
+  it 'should add an array of dices to array set' do
+    dice_set.add([dice, dice, dice]).count.should eq(3)
+  end
+
+  it 'should take dice from diceset' do
+    source = dice_set_with([1, 2, 3, 4])
+    destination = source.take([0,2])
+
+    destination.should eq(dice_set_with([1, 3]))
+    source.should eq(dice_set_with([2,4]))
+  end
+
+  it 'should merge dice sets' do
+    dice_set1 = dice_set_with([1,4])
+    dice_set2 = dice_set_with([3,2])
+
+    merged_dice_set = dice_set1 + dice_set2
+
+    merged_dice_set.should eq(dice_set_with([1,4,3,2]))
+  end
+
+
 
   private
   def dice_set_with(faces)
