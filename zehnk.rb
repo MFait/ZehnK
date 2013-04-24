8c9540e6cd1f2e78d2997107136a26ae8937e62f
Felix Kitaka <webash.uganda@gmail.com>
2013-04-24 10:40:37 +0300

    implement game roll and take

--- zehnk.rb
+++ zehnk.rb
@@ -10,13 +10,25 @@ set :session_secret, 'So0perSeKr3t!'
 
 
 get '/' do
-  game = game_session
-  #game.start
-  #game.pocket([1,2])
-  haml :index, :locals => { :game => game}
+  haml :index, :locals => { :game => current_game}
 end
 
-def game_session
+get '/reset' do
+  session.clear if session?
+  redirect '/'
+end
+
+get '/take' do
+  current_game.pocket([1,2])
+  redirect '/'
+end
+
+get '/roll' do
+  current_game.roll
+  redirect '/'
+end
+
+def current_game
   session_start! unless session?
   session["game"] = Game.new.start unless session["game"]
 
