8146679b167fd4bf5406c73b494b049ef2acce6a
Felix Kitaka <webash.uganda@gmail.com>
2013-04-24 13:48:59 +0300

    Added the score class

---
+++ lib/score.rb
@@ -0,0 +1,39 @@
+class Score
+
+  def initialize
+    @dices = []
+  end
+
+  def calculate(dice_set)
+    @dices = dice_set.dices
+    sum = 0
+
+    sum += 100 if single?(1)
+    sum += 200 if double?(1)
+    sum += 50 if single?(5)
+    sum += 100 if double?(5)
+
+    [2, 3,4,5,6].each { |value|
+      sum += value * 100 if triple?(value)
+    }
+
+    sum
+  end
+
+  def number_of(face)
+    @dices.select { |dice| dice.face==face}.length
+  end
+
+  def single?(face)
+    number_of(face) == 1
+  end
+
+
+  def double?(face)
+    number_of(face) == 2
+  end
+
+  def triple?(face)
+    number_of(face) == 3
+  end
+end
