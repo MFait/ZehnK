34e3fcf1777fabc0b1e1f0de96e94e17c0e7af52
Felix Kitaka <webash.uganda@gmail.com>
2013-04-24 10:28:23 +0300

    Added a helpers file to manage the helpers

---
+++ helpers/view_helpers.rb
@@ -0,0 +1,33 @@
+require './lib/dice'
+require './lib/dice_set'
+
+helpers do
+  def display_table_dice_set(dice_set)
+    output = []
+    dice_set.dices.each_with_index do |dice, index|
+      output << display_dice_with_checkbox(dice, index)
+    end
+    output.join ''
+  end
+
+  def display_pocket_dice_set(dice_set)
+    output = []
+    dice_set.dices.each do |dice|
+      output << display_dice(dice)
+    end
+    output.join ''
+  end
+
+  def display_dice(dice)
+    "<div class='dice_image'>
+    <img src='images/dice_#{dice.face}.png'/>
+    </div>"
+  end
+
+  def display_dice_with_checkbox(dice, index)
+    "<div class='dice_image'>
+    <input type='checkbox' id='img_#{index}'/>
+    <label for='img_#{index}'><img src='images/dice_#{dice.face}.png'/></label>
+    </div>"
+  end
+end
--- zehnk.rb
+++ zehnk.rb
@@ -4,39 +4,10 @@ require './lib/dice_set'
 require './lib/game'
 require 'haml'
 require 'sinatra/session'
+require './helpers/view_helpers'
 
 set :session_secret, 'So0perSeKr3t!'
 
-helpers do
-  def display_table_dice_set(dice_set)
-    output = []
-    dice_set.dices.each_with_index do |dice, index|
-       output << display_dice_with_checkbox(dice, index)
-    end
-    output.join ''
-  end
-
-  def display_pocket_dice_set(dice_set)
-    output = []
-    dice_set.dices.each do |dice|
-      output << display_dice(dice)
-    end
-    output.join ''
-  end
-
-  def display_dice(dice)
-    "<div class='dice_image'>
-    <img src='images/dice_#{dice.face}.png'/>
-    </div>"
-  end
-
-  def display_dice_with_checkbox(dice, index)
-    "<div class='dice_image'>
-    <input type='checkbox' id='img_#{index}'/>
-    <label for='img_#{index}'><img src='images/dice_#{dice.face}.png'/></label>
-    </div>"
-  end
-end
 
 get '/' do
   game = game_session
